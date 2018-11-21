9660b37e25ca6dfed8e1f566b793b14c368b457c
Attila Szűcs <a.szunyi@gmail.com>
2018-11-21 09:44:28 +0100

    KAnalyse Script

--- KmerAnalyse/Form1.Designer.vb
+++ KmerAnalyse/Form1.Designer.vb
@@ -22,10 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ScriptsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreateKAnalyseScriptToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ScriptsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ScriptsToolStripMenuItem
+        '
+        Me.ScriptsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreateKAnalyseScriptToolStripMenuItem})
+        Me.ScriptsToolStripMenuItem.Name = "ScriptsToolStripMenuItem"
+        Me.ScriptsToolStripMenuItem.Size = New System.Drawing.Size(65, 24)
+        Me.ScriptsToolStripMenuItem.Text = "Scripts"
+        '
+        'CreateKAnalyseScriptToolStripMenuItem
+        '
+        Me.CreateKAnalyseScriptToolStripMenuItem.Name = "CreateKAnalyseScriptToolStripMenuItem"
+        Me.CreateKAnalyseScriptToolStripMenuItem.Size = New System.Drawing.Size(233, 26)
+        Me.CreateKAnalyseScriptToolStripMenuItem.Text = "Create KAnalyse Script"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ScriptsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreateKAnalyseScriptToolStripMenuItem As ToolStripMenuItem
 End Class
--- KmerAnalyse/Form1.vb
+++ KmerAnalyse/Form1.vb
@@ -1,3 +1,25 @@
-﻿Public Class Form1
+﻿Imports Szunyi.IO.FilePath_Conversion
+Public Class Form1
+    Private Sub CreateKAnalyseScriptToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreateKAnalyseScriptToolStripMenuItem.Click
+        ' Get Files
+        ' Get Kmer Length
+        ' Get Output Directory
+        Dim Files = Szunyi.IO.Pick_Up.Fasta_Fastq().ToList
+        If Files.Count = 0 Then Exit Sub
+        Dim Dir = Szunyi.IO.Pick_Up.Directory("Select Output Directory")
+        If IsNothing(Dir) = True Then Exit Sub
+        Dim Kmers = Szunyi.Common.MyInputBox.GetIntegers("Enter interesting Kmer lengths Separated By space")
+        If Kmers.Count = 0 Then Exit Sub
 
+        Dim str As New System.Text.StringBuilder
+        For Each Kmer In Kmers
+            For Each File In Files
+                str.Append("count ").Append(File.FullName_Windows_cmd).Append(" -k ").Append(Kmer).Append(" -o ")
+                Dim oFile = Szunyi.IO.Rename.Merge(Dir, File.Name).ChangeExtension(".kc")
+                oFile = oFile.Append_Before_Extension("_" & Kmer)
+                str.Append(oFile.FullName_Windows_cmd).AppendLine()
+            Next
+        Next
+        Clipboard.SetText(str.ToString)
+    End Sub
 End Class
