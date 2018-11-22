a2a3f6dd12f5a642d178607a3f5599909b560b6a
Attila Szűcs <a.szunyi@gmail.com>
2018-11-22 10:47:17 +0100

    Filter

--- KmerAnalyse/Form1.vb
+++ KmerAnalyse/Form1.vb
@@ -1,4 +1,5 @@
-﻿Imports Szunyi.IO
+﻿Imports System.IO
+Imports Szunyi.IO
 Imports Szunyi.IO.FilePath_Conversion
 Public Class Form1
     Private Sub CreateKAnalyseScriptToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreateKAnalyseScriptToolStripMenuItem.Click
@@ -38,8 +39,101 @@ Public Class Form1
         Dim x As New KAnalyseFilterSetting
         Dim t As New Szunyi.IO.Controls.Set_Console_Properties(x)
         If t.ShowDialog() <> DialogResult.OK Then Exit Sub
+        Dim Files = Szunyi.IO.Pick_Up.Files(Szunyi.IO.File_Extensions.All_TAB_Like, "Every File is different Group. FileNames in File are separated by Enter without Header").ToList
+        If Files.Count = 0 Then Exit Sub
+        Dim File = Szunyi.IO.Pick_Up.File(Szunyi.IO.File_Extensions.KAnalyse)
+        If IsNothing(File) = True Then Exit Sub
+        Dim Header = Szunyi.IO.Import.Text.GetHeader(File, 1)
+        Dim gr As New Groups(Header, Files)
+        For Each Line In Szunyi.IO.Import.Text.ParseNotFirst(File, 1)
+            Dim c = gr.GetCounts(Line)
+            If x.MinSumInAll.Default_Value <= c.AllCounts Then
+                For i1 = 0 To c.GroupCounts.Count - 1
+                    ' Check is Any of Group is passed
+                    If c.GroupCounts(i1) >= x.MinSumInGroup.Default_Value Then
+                        If c.GroupPercents(i1) >= x.MinPercentOfGroup.Default_Value Then
+                            If c.nofPositiveSampleInGroup(i1) >= x.MinPositiveInGroup.Default_Value Then
+                                gr.SWs(i1).Write(">" & c.ToName(i1) & vbCrLf & c.Seq & vbCrLf)
+                            End If
+                        End If
+                    End If
+                Next
+            End If
+        Next
+        For Each sw In gr.SWs
+            sw.Flush()
+            sw.Close()
 
+        Next
+        Dim kj As Int16 = 54
 
 
     End Sub
+    Private Class Groups
+        Private header As List(Of String)
+        Private files As List(Of FileInfo)
+        Public GroupIndexes As New List(Of Group)
+        Public Property SWs As New List(Of StreamWriter)
+        Public Sub New(header As List(Of String), files As List(Of FileInfo))
+            Me.header = header
+            Me.files = files
+            Dim cIndex As Integer = 0
+            Dim GrIndex As Integer = 0
+            For Each File In files
+                SWs.Add(New StreamWriter(File.ChangeExtension(Szunyi.IO.File_Extension.fasta).FullName))
+                Dim GrName = File.woExtension
+                Dim FileNames = Szunyi.IO.Import.Text.Parse(File).ToList
+                For i1 = 1 To FileNames.Count
+                    Dim g As New Group(i1 + cIndex, FileNames(i1 - 1), GrName.Name, GrIndex)
+                    GroupIndexes.Add(g)
+                    GrIndex += 1
+                Next
+                cIndex += FileNames.Count
+            Next
+
+        End Sub
+        Public Function GetCounts(Line As String) As Counts
+            Dim t = Split(Line, vbTab)
+            Dim c As New Counts
+            c.Seq = t.First
+            ReDim c.GroupCounts(GroupIndexes.Count - 1)
+            ReDim c.GroupPercents(GroupIndexes.Count - 1)
+            ReDim c.nofPositiveSampleInGroup(GroupIndexes.Count - 1)
+            For Each Item In Me.GroupIndexes
+                c.GroupCounts(Item.GroupIndex) += t(Item.Index)
+                If t(Item.Index) <> 0 Then c.nofPositiveSampleInGroup(Item.GroupIndex) += 1
+            Next
+            c.AllCounts = c.GroupCounts.Sum
+            For i1 = 0 To GroupIndexes.Count - 1
+                c.GroupPercents(i1) = c.GroupCounts(i1) / c.AllCounts * 100
+            Next
+            Return c
+        End Function
+    End Class
+    Private Class Group
+
+        Public Sub New(Index As Integer, FileName As String, GroupName As String, GrIndex As Integer)
+            Me.Index = Index 'ColumnIndex
+            Me.GroupName = GroupName
+            Me.FileName = FileName
+            Me.GroupIndex = GrIndex
+        End Sub
+
+        Public Property Index As Integer ' ColumnNumber
+        Public Property GroupName As String
+        Public Property GroupIndex As Integer
+        Public Property FileName As String
+    End Class
+
+    Private Class Counts
+        Public Property Seq As String
+        Public Property AllCounts As Integer
+        Public Property GroupCounts As Integer()
+        Public Property GroupPercents As Double()
+        Public Property nofPositiveSampleInGroup As Integer()
+        Public Function ToName(Index As Integer) As String
+            Return Seq & "_" & GroupCounts(Index) & "_" & GroupPercents(Index) & "_" & nofPositiveSampleInGroup(Index)
+        End Function
+
+    End Class
 End Class
